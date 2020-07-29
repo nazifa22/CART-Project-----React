b35724808f9c89706f6c955bf6812bb84c3568d4
nazifa <nazifarashid1822@gmail.com>
2020-07-29 21:25:29 +0600

    Shop page & header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 import Homepage from './Components/Pages/homepage/homepage';
 import { Route, Switch } from 'react-router-dom';
 import ShopComponent from './Components/Pages/shop/shopComponent';
+import Header from './Components/Header/header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopComponent} />
