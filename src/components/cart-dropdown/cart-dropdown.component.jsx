f6729ebde76f6ffd30a5ae409346a957a58d43a3
nazifa <nazifarashid1822@gmail.com>
2020-08-05 20:58:10 +0600

    Added reselect and cart counter

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -9,9 +9,11 @@ import './cart-dropdown.styles.scss';
 const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
-      {cartItems.map(cartItem => (
-        <CartItem key={cartItem.id} item={cartItem} />
-      ))}
+      {
+        cartItems.map(cartItem => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))
+      }
     </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
